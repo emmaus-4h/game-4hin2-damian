d39f5cc3a08aa31c54ac599c0c43a400c6421fe0
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2022-05-24 16:55:35 +0200

    Uit repo van Yosei en Damian

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ var spelStatus = SPELEN;
 
 var spelerX = 600; // x-positie van speler
 var spelerY = 600; // y-positie van speler
+var aantal = 9;
+
+var vijandX = 600;
+var vijandY = 100;
+
+var kogelX = spelerX - 15;
+var kogelY = spelerY - 45;
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -26,10 +33,33 @@ var spelerY = 600; // y-positie van speler
  */
 var beweegAlles = function () {
   // speler
-
+  if (keyIsDown(68)) {
+spelerX = spelerX +3;
+  }
+  if (keyIsDown(65)) {
+spelerX = spelerX -3;
+  }
+  if (keyIsDown(87)) {
+spelerY = spelerY -3;
+  }
+  if (keyIsDown(83)) {
+spelerY = spelerY +3;
+  }
   // vijand
+  vijandX = vijandX -5;
+  if (vijandX < 0){
+    vijandX=1280;
+  }
 
   // kogel
+if (mouseIsPressed) {
+  kogelY= spelerY;
+  kogelX=spelerX;
+}
+  kogelY = kogelY -9;
+  if (kogelY < 0) {
+    kogelY=0;
+  }
 };
 
 /**
@@ -41,7 +71,12 @@ var verwerkBotsing = function () {
   // botsing speler tegen vijand
 
   // botsing kogel tegen vijand
-
+    if (kogelX - vijandX < 50 &&
+      kogelX - vijandX >-50 &&
+      kogelY - vijandY <50 &&
+      kogelY - vijandY > -50) {
+    console.log("Botsing");
+      }
   // update punten en health
 
 };
@@ -51,17 +86,22 @@ var verwerkBotsing = function () {
  */
 var tekenAlles = function () {
   // achtergrond
-
+  fill("blue")
+  rect(0,0,1280,720)
   // vijand
-
+  fill("red");
+  rect(vijandX - 25, vijandY - 25, 50, 50)
+  
+  
   // kogel
-
+  fill("black");
+  rect(kogelX, kogelY, 30, 30)
   // speler
   fill("white");
   rect(spelerX - 25, spelerY - 25, 50, 50);
   fill("black");
   ellipse(spelerX, spelerY, 10, 10);
-
+  
   // punten en health
 
 };
@@ -71,6 +111,15 @@ var tekenAlles = function () {
  * anders return false
  */
 var checkGameOver = function () {
+    if (spelerX - vijandX < 50 &&
+      spelerX - vijandX >-50 &&
+      spelerY - vijandY <50 &&
+      spelerY - vijandY > -50) {
+    console.log("Botsing");
+      return true;
+      }
+  
+  
   // check of HP 0 is , of tijd op is, of ...
   return false;
 };
@@ -107,6 +156,7 @@ function draw() {
     }
   }
   if (spelStatus === GAMEOVER) {
+    console.log("game over")
     // teken game-over scherm
 
   }
